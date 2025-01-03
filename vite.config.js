fba58fd36179551d2798f6e623f83805a5c7a48f
Jorge Oehrens <oehrens@Jorges-MacBook-Pro.local>
2025-01-02 21:17:07 -0300

    bug: edit package.json

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,26 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import svgr from 'vite-plugin-svgr';
+import react from '@vitejs/plugin-react-swc';
 import path from 'path';
+import nodePolyfills from 'vite-plugin-node-stdlib-browser';
+import eslint from 'vite-plugin-eslint';
+import svgr from 'vite-plugin-svgr';
+
+// https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [
-        react(),
-        svgr() // Agrega el plugin aquí
-    ],
-    resolve: {
-        alias: {
-            '@': path.resolve(__dirname, './src')
-        }
-    }
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  server: {
+    host: '0.0.0.0',
+    port: 3000,
+  },
+  preview: {
+    port: 3000,
+    host: true,
+  },
+  plugins: [svgr(), react(), nodePolyfills(), eslint()],
+  assetsInclude: ['**/*.wasm?inline', '**/*.txt?inline'],
+  build: { outDir: 'build' },
 });
