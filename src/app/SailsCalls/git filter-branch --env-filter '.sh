c3cdc1a8296f008543a24b2c3782b7cfa04edc9b
Jorge Oehrens <jorge.oehrens@gmail.com>
2025-01-04 01:40:58 -0300

    update

---
+++ src/app/SailsCalls/git filter-branch --env-filter '.sh
@@ -0,0 +1,15 @@
+git filter-branch --env-filter '
+OLD_EMAIL="jorge.oehrens@aulmos.ucentral.cl"
+CORRECT_NAME="Jorge Oehrens"
+CORRECT_EMAIL="jorge.oehrens@gmail.com"
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
